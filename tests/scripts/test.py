07a81902473d9ab945cf58a42c4bb916ca41801f
vitor batista <vitor.shofar@gmail.com>
2024-11-30 20:20:31 -0300

    chore: configure owlapi python module for tests

---
+++ tests/scripts/test.py
@@ -0,0 +1,8 @@
+from owlapy.owl_ontology_manager import SyncOntologyManager
+
+ontology_path = "/home/vbatista/estudo/ontologias/datamodel/owl/DataModel.owl"
+onto = SyncOntologyManager().load_ontology(ontology_path)
+
+
+
+
