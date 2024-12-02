e941cbc14540d8c92efde8955c55fb027cd4c245
vitor batista <vitor.shofar@gmail.com>
2024-12-02 01:07:07 -0300

    feat: create genral inferences python script

--- scripts/python/infer.py
+++ scripts/python/infer.py
@@ -1,19 +1,14 @@
 from owlapy.iri import IRI
 from owlapy.owl_ontology_manager import OntologyManager
+from owlapy.owl_reasoner import SyncReasoner
+from owlapy.static_funcs import stopJVM
 
-manager = OntologyManager()
-onto = manager.load_ontology(IRI.create("file:///home/vbatista/estudo/ontologias/datamodel/owl/datamodel-owl.owl"))
-
-
-'''
+ontology_path = "/home/vbatista/estudo/ontologias/datamodel/owl//datamodel-owl.owl"
 sync_reasoner = SyncReasoner(ontology = ontology_path, reasoner="HermiT")
 onto = OntologyManager().load_ontology(ontology_path)
 
 for i in onto.classes_in_signature():
-
     instances=sync_reasoner.instances(i,direct=False)
     print(f"Class:{i}\t Num instances:{len(instances)}")
 
 stopJVM()
-'''
-
