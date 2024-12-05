dc734f1b45bf3e6b18d7e83afea0670cabaf285a
vitor batista <vitor.shofar@gmail.com>
2024-12-04 22:38:13 -0300

    test: replace test structure

---
+++ scripts/python/tests/entity_tests.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python 
+
+from owlready2 import *
+
+def remove_all_individuals(onto):
+    for i in list(onto.individuals()):
+        destroy_entity(i)
+
+def print_all_individuals(onto):
+    print("Print all individuals..")
+    for i in onto.individuals():
+        print(i)
+
+def load_ontology():        
+    onto = get_ontology("/home/vbatista/estudo/ontologias/datamodel/owl/DataModel.owl").load()
+    return onto
+
+
+def given_employee_haskey_ssn_infer(onto):
+    remove_all_individuals(onto)        
+
+    employee = Thing("employee",onto)
+    ssn = Thing("ssn", onto)
+    employee.hasKey.append(ssn)
+
+    inferences = get_ontology("http://tests/Inferences.owl")
+
+
+    with inferences:
+        sync_reasoner_pellet(infer_property_values = True, infer_data_property_values = True)
+        
+
+    assert(employee in onto.Entity.instances())
+    assert(employee in onto.StrongEntity.instances())
+    assert(employee in ssn.isAttributeOf)
+    assert(employee in ssn.isKeyOf )
+    assert(ssn in onto.employee.hasAttribute)
+
+
+onto = load_ontology()
+given_employee_haskey_ssn_infer(onto)
+
+
+
+
+
+
+
+
+
+
+
+
