0f3864a70f06c521e0a041747a901bbb34232193
vitor batista <vitor.shofar@gmail.com>
2024-12-04 22:15:35 -0300

    test: improve initial test structure

--- scripts/python/utils/infer.py
+++ scripts/python/utils/infer.py
@@ -14,7 +14,6 @@ def print_all_individuals(onto):
     for i in onto.individuals():
         print(i)
 
-
 remove_all_individuals(onto)        
 
 employee = Thing("employee",onto)
@@ -23,20 +22,20 @@ employee.hasKey = [ssn]
 
 inferences = get_ontology("http://tests/Inferences.owl")
 
+
 with inferences:
     sync_reasoner_pellet(infer_property_values = True, infer_data_property_values = True)
     
 
-assert(onto.employee.is_a == [onto.StrongEntity])
-assert(onto.ssn.is_a == [onto.Attribute])
+assert(onto.employee in onto.Entity.instances())
+assert(onto.employee in onto.StrongEntity.instances())
 assert(onto.employee.hasAttribute == [onto.ssn])
 assert(onto.ssn.isAttributeOf == [onto.employee])
 assert(onto.ssn.isKeyOf == [onto.employee])
 
 
-print(inferences.get_parents_of(onto.employee))
 
-inferences.save("/home/vbatista/estudo/ontologias/datamodel/owl/Inferences.owl")        
+
 
 
 
