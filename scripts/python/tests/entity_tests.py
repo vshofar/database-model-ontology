4f9f677a3cdce04b51b4c1cf478bfe83a6da1d1e
vitor batista <vitor.shofar@gmail.com>
2024-12-05 00:32:04 -0300

    test: add hasAttribute inference tests

--- scripts/python/tests/entity_tests.py
+++ scripts/python/tests/entity_tests.py
@@ -5,22 +5,36 @@ from utils import *
 
 class TestEntity(unittest.TestCase):
 
-    def test_given_employee_haskey_ssn_infer(self):
+    def test_given_thing_hasAttribute_thing_should_infer(self):
 
         onto = load_ontology()
 
         remove_all_individuals(onto)        
         
-        entity = onto.Entity
-        strongEntity = onto.StrongEntity
+        employee = Thing("employee",onto)
+        salary = Thing("salary", onto)
+        employee.hasAttribute.append(salary)    
+
+        sync_reasoner()        
+
+        self.assertIn(salary, onto.Attribute.instances())        
+        self.assertIn(employee, salary.isAttributeOf)        
+        
+
+    def test_given_thing_haskey_thing_should_infer(self):
+
+        onto = load_ontology()
+
+        remove_all_individuals(onto)        
+        
         employee = Thing("employee",onto)
         ssn = Thing("ssn", onto)
         employee.hasKey.append(ssn)    
 
         sync_reasoner()        
 
-        self.assertIn(employee, entity.instances())        
-        self.assertIn(employee, strongEntity.instances())        
+        self.assertIn(employee, onto.Entity.instances())        
+        self.assertIn(employee, onto.StrongEntity.instances())        
         self.assertIn(employee, ssn.isAttributeOf)
         self.assertIn(employee, ssn.isKeyOf )
         self.assertIn(ssn, employee.hasAttribute) 
