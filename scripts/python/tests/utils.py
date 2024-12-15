09ddfee8a3847f2e0db76f2917bbdb541458a16c
vitor batista <vitor.shofar@gmail.com>
2024-12-15 14:37:18 -0300

    chore: update ontology path

--- scripts/python/tests/utils.py
+++ scripts/python/tests/utils.py
@@ -3,5 +3,4 @@ from owlapy.owl_reasoner import SyncReasoner
 
 def load_reasoner(name):
     ontology_path = "/home/vbatista/estudo/ontologias/datamodel/owl/DataModel.owl"
-    ontology_path = "/home/vbatista/estudo/ontologias/datamodel/owl/DataModel.ofn"
     return SyncReasoner(ontology=ontology_path, reasoner=name)
