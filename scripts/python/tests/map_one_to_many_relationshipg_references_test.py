f1a1a4742ed6ad17ef1f21f11974472bf8fbe353
vitor batista <vitor.shofar@gmail.com>
2025-01-12 17:08:02 -0300

    chore: map one to many relationship target reference

--- scripts/python/tests/map_one_to_many_relationshipg_references_test.py
+++ scripts/python/tests/map_one_to_many_relationshipg_references_test.py
@@ -67,6 +67,36 @@ class TestRelationalRules(unittest.TestCase):
         self.assertTrue(scenario.evaluate(q3))
         self.assertTrue(scenario.evaluate(q4))
 
+    def test_given_one_to_many_relationship_relation_target_reference(self):
+
+        scenario = (
+            self._assert
+            .object_property_value("employee", "hasKey", "ssn")
+            .object_type("dependsOfEmployeeParticipation", "OneCardinalityRelationshipParticipation")
+            .object_property_value("dependsOfEmployeeParticipation", "hasParticipationEntity", "employee")
+            .object_property_value("dependent", "hasAttribute", "name")
+            .object_type("dependsOfDependentParticipation", "ManyCardinalityRelationshipParticipation")
+            .object_type("dependsOfDependentParticipation", "TotalRelationshipParticipation")
+            .object_property_value("dependsOfDependentParticipation", "hasParticipationEntity", "dependent")
+            .object_property_value("dependsOf", "hasParticipation", "dependsOfEmployeeParticipation")
+            .object_property_value("dependsOf", "hasParticipation", "dependsOfDependentParticipation")
+            .object_property_only_with_individuals("dependsOf", "hasParticipation", ["dependsOfEmployeeParticipation",
+                                                                                     "dependsOfDependentParticipation"])
+        )
+
+        q1 = self._query.hasType("dependsOfEmployeeParticipation", "ReferenceParticipation")
+        q2 = self._query.hasPropertyValue("dependsOfEmployeeParticipation", "hasReferenceParticipationRelation",
+                                          "employee")
+        q3 = self._query.hasPropertyValue("dependsOfEmployeeParticipation",
+                                          "hasReferenceParticipationRelationAttribute", "ssn")
+        q4 = self._query.hasPropertyValue("dependsOfEmployeeParticipation", "hasReferenceParticipationSide",
+                                          "targetReferenceParticipationSide")
+
+        self.assertTrue(scenario.evaluate(q1))
+        self.assertTrue(scenario.evaluate(q2))
+        self.assertTrue(scenario.evaluate(q3))
+        self.assertTrue(scenario.evaluate(q4))
+
 
 
 
